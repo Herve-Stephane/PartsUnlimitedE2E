21843f5875c781d4e1c2b3f74291f05731bcf2db
herve-stephane <herve_ste@yahoo.fr>
2020-07-27 10:12:18 +0200

    test commit

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -5,8 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
     {
-        // Test commit Herve-Stephane!
-
+        // Test commit Herve-Stephane! on git
 
         [Key]
         public int CartItemId { get; set; }
